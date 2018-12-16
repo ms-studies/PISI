df37771debb757a2d04a62e34640d51cc2bb3b9f
Marcin Szałek <marcin.szalek1@gmail.com>
2018-12-16 21:16:29 +0100

    Done kNN recommendations

--- recommendations/data_loader.py
+++ recommendations/data_loader.py
@@ -1,17 +1,53 @@
 import pandas as pd
+import urllib.request
+import json
+from collections import namedtuple
+from time import sleep
 
-def main():
+
+def _json_object_hook(d): return namedtuple('X', d.keys())(*d.values())
+def json2obj(data): return json.loads(data, object_hook=_json_object_hook)
+
+def loadMoviesData():
     colnames = ['id', 'webid', 'name']
-
-    data = pd.read_csv('movie.csv', names = colnames, sep=';')
-    # print(data)
+    data = pd.read_csv('data/movie.csv', names = colnames, sep=';')
+    return data
 
 def loadTaskData():
     colnames = ['id', 'personId', 'movieId', 'review']
-
     data = pd.read_csv('data/task.csv', names = colnames, sep=';')
     return data
 
+def loadTrainData():
+    colnames = ['id', 'personId', 'movieId', 'review']
+    data = pd.read_csv('data/train.csv', names = colnames, sep=';')
+    return data
+
+def loadMovieDetails():
+    cols = ['intId', 'webId', 'details']
+    data = pd.read_csv('data/movie_details.csv', names = cols, sep=';')
+    map = {}
+    for index, row in data.iterrows():
+        map[row['intId']] = json2obj(row['details'])
+    
+    return map
+
+def getDataFromApi(data):
+    cols = ['intId', 'webId', 'details']
+    apidataframe = pd.DataFrame(columns = cols)
+    for index, row in data.iterrows():
+        id = row['webid']
+        contents = urllib.request.urlopen(f"https://api.themoviedb.org/3/movie/{id}?api_key=42aa0017f58528179d15a0ec047d7a26&language=en-US").read().decode('utf-8')
+        apidataframe.loc[len(apidataframe)] = [row['id'], row['webid'], contents]
+        sleep(0.3)
+
+    apidataframe.to_csv('data/movie_details.csv', sep=';', index=False, header=False)
+
+    return data
+
+def main():
+    data = loadMoviesData()
+    data = getDataFromApi(data)
 
 if __name__ == '__main__':
     main()
--- recommendations/trainer.py
+++ recommendations/trainer.py
@@ -1,11 +1,46 @@
-from data_loader import loadTaskData
+from data_loader import loadTaskData, loadTrainData, loadMovieDetails
 import numpy as np
+from collections import Counter
 
 def main():
+    trainData = loadTrainData()
+    movieDetails = loadMovieDetails()
+    
+    
+    k = 8
+    f = open("submission.csv","w+")
+
     taskData = loadTaskData()
-    taskData['review'] = np.random.randint(0, 6, taskData.shape[0])
-    # print(taskData)
-    taskData.to_csv('submission.csv', sep=';', index=False, header=False)
+    for index, row in taskData.iterrows():
+        personId = row['personId']
+        movieId = row['movieId']
+        investigatedMovie = movieDetails[movieId]
+        
+        personChoices = trainData.loc[trainData['personId'] == personId]
+        similarities = []
+        for idx, choice in personChoices.iterrows():
+            knownMovie = movieDetails[choice['movieId']]
+            similarityScore = similarity(knownMovie, investigatedMovie)
+            similarities.append((similarityScore, choice['review']))
+
+        similarities.sort(reverse = True)
+        nearestNeighbors = similarities[:k]
+        nearestLabels = [x[1] for x in nearestNeighbors]
+        review = max(set(nearestLabels), key=nearestLabels.count)
+
+        stringRow = f"{int(row['id'])};{int(personId)};{int(movieId)};{review}\n"
+        f.write(stringRow)
+
+    f.close()
+ 
+
+def similarity(movie1, movie2):
+    sameGenresCount = len(list(set(movie1.genres).intersection(movie2.genres)))
+    languagesMatchPoint = 1 if movie1.original_language == movie2.original_language else 0
+    # popularityMatch = 
+    similarity = 3 * sameGenresCount + languagesMatchPoint
+    return similarity
+
 
 if __name__ == '__main__':
     main()
