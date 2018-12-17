f07d3892db303f2648f409792d352d31a683f892
Marcin Szałek <marcin.szalek1@gmail.com>
2018-12-17 08:14:25 +0100

    Improved kNN recommendations

--- recommendations/data_loader.py
+++ recommendations/data_loader.py
@@ -37,7 +37,7 @@ def getDataFromApi(data):
     apidataframe = pd.DataFrame(columns = cols)
     for index, row in data.iterrows():
         id = row['webid']
-        contents = urllib.request.urlopen(f"https://api.themoviedb.org/3/movie/{id}?api_key=42aa0017f58528179d15a0ec047d7a26&language=en-US").read().decode('utf-8')
+        contents = urllib.request.urlopen(f"https://api.themoviedb.org/3/movie/{id}?api_key=42aa0017f58528179d15a0ec047d7a26&language=en-US&append_to_response=keywords").read().decode('utf-8')
         apidataframe.loc[len(apidataframe)] = [row['id'], row['webid'], contents]
         sleep(0.3)
 
--- recommendations/trainer.py
+++ recommendations/trainer.py
@@ -1,13 +1,14 @@
 from data_loader import loadTaskData, loadTrainData, loadMovieDetails
 import numpy as np
 from collections import Counter
+import random
+import pandas as pd
 
 def main():
     trainData = loadTrainData()
     movieDetails = loadMovieDetails()
     
-    
-    k = 8
+    k = 38
     f = open("submission.csv","w+")
 
     taskData = loadTaskData()
@@ -28,17 +29,73 @@ def main():
         nearestLabels = [x[1] for x in nearestNeighbors]
         review = max(set(nearestLabels), key=nearestLabels.count)
 
+        # if index == 0:
+            # break
+
         stringRow = f"{int(row['id'])};{int(personId)};{int(movieId)};{review}\n"
         f.write(stringRow)
 
     f.close()
- 
+
+
+def test():
+    trainData = loadTrainData()
+    movieDetails = loadMovieDetails()
+    
+    trainData, validationData = splitData(trainData)
+    # taskData = loadTaskData()
+
+    diffs = [ [0] * 6 for _ in range(61)]
+    count = 0
+    
+    print("starting iterations")
+    for index, row in validationData.iterrows():
+        personId = row['personId']
+        movieId = row['movieId']
+        investigatedMovie = movieDetails[movieId]
+        
+        personChoices = trainData.loc[trainData['personId'] == personId]
+        similarities = []
+        for idx, choice in personChoices.iterrows():
+            knownMovie = movieDetails[choice['movieId']]
+            similarityScore = similarity(knownMovie, investigatedMovie)
+            similarities.append((similarityScore, choice['review']))
+
+        similarities.sort(reverse = True)
+
+        for k in range(1, 61):
+            nearestNeighbors = similarities[:k]
+            nearestLabels = [x[1] for x in nearestNeighbors]
+            review = max(set(nearestLabels), key=nearestLabels.count)
+            
+            diff = abs(row['review'] - review)
+            diffs[k][diff] += 1
+        count += 1
+        
+    
+    for k in range(1, 61):
+        print(f"{k}   {diffs[k][0]/count}   {(diffs[k][0]+diffs[k][1])/count}")
+
+def splitData(data):
+    colnames = ['id', 'personId', 'movieId', 'review']
+    validation = pd.DataFrame(columns = colnames)
+    train = pd.DataFrame(columns = colnames)
+
+    for idx, row in data.iterrows():
+        if idx % 4 == 0:
+            validation.loc[len(validation)] = row
+        else:
+            train.loc[len(train)] = row
+        
+    return train, validation
 
 def similarity(movie1, movie2):
     sameGenresCount = len(list(set(movie1.genres).intersection(movie2.genres)))
+    sameKeywordsCount = len(list(set(movie1.keywords.keywords).intersection(movie2.keywords.keywords)))
     languagesMatchPoint = 1 if movie1.original_language == movie2.original_language else 0
-    # popularityMatch = 
-    similarity = 3 * sameGenresCount + languagesMatchPoint
+    voteDifference = (movie1.vote_average - movie2.vote_average) * (movie1.vote_average - movie2.vote_average)
+
+    similarity = 3 * sameGenresCount + 2 * sameKeywordsCount + languagesMatchPoint - 2 * voteDifference
     return similarity
 
 
